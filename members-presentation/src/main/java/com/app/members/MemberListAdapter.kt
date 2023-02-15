02b484b7de9c347c042a38b24f300ba763e71e46
Oscar Ekesiobi <oscarnipps@gmail.com>
2023-02-15 16:45:41 +0100

    member list refactor with list adapter and data binding

--- members-presentation/src/main/java/com/app/members/MemberListAdapter.kt
+++ members-presentation/src/main/java/com/app/members/MemberListAdapter.kt
@@ -1,19 +1,19 @@
 package com.app.members
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.app.members_domain.model.Member
 import com.app.teamly.members_presentation.R
 import com.app.teamly.members_presentation.databinding.MemberListItemBinding
 import com.bumptech.glide.Glide
 
-class MemberListAdapter() : RecyclerView.Adapter<MemberListAdapter.MemberListViewHolder>() {
-
-    private var membersList = emptyList<Member>()
+class MemberListAdapter(val memberItemListener: MemberItemListener) :
+    ListAdapter<Member, MemberListAdapter.MemberListViewHolder>(MemberListItemDiffCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MemberListViewHolder {
         val binding: MemberListItemBinding = DataBindingUtil.inflate(
@@ -27,39 +27,50 @@ class MemberListAdapter() : RecyclerView.Adapter<MemberListAdapter.MemberListVie
     }
 
     override fun onBindViewHolder(holder: MemberListViewHolder, position: Int) {
-        val currentMember = membersList[position]
+        val currentMember = getItem(position)
         holder.bindMember(currentMember)
     }
 
-    override fun getItemCount(): Int {
-        return membersList.size
+    interface MemberItemListener {
+        fun onMemberItemClicked(currentItem: Member)
+    }
+
+    private class MemberListItemDiffCallback : DiffUtil.ItemCallback<Member>() {
+        override fun areItemsTheSame(oldItem: Member, newItem: Member): Boolean {
+            return oldItem.email == newItem.email
+        }
+
+        override fun areContentsTheSame(oldItem: Member, newItem: Member): Boolean {
+            return oldItem == newItem
+        }
     }
 
     inner class MemberListViewHolder(private val itemBinding: MemberListItemBinding) :
-        ViewHolder(itemBinding.root) {
+        ViewHolder(itemBinding.root) , View.OnClickListener {
+
+        init {
+            itemView.setOnClickListener(this)
+        }
 
         fun bindMember(member: Member) {
-            itemBinding.memberName.text = member.name
+            itemBinding.member = member
 
-            itemBinding.memberEmail.text = member.email
-
-            itemBinding.teamsCount.text = member.teamsCount.toString()
+            itemBinding.executePendingBindings()
 
             //todo: change to use binding adapter
             Glide.with(itemView.context)
                 .load(member.imageUrl)
-                .into(itemBinding.memberImage);
+                .into(itemBinding.memberImage)
+
+            //todo: change to use binding adapter
+            itemBinding.teamsCount.text = member.teamsCount.toString()
+
         }
 
-    }
+        override fun onClick(p0: View?) {
+            val currentItem = getItem(bindingAdapterPosition)
+            memberItemListener.onMemberItemClicked(currentItem)
+        }
 
-    fun setMembers(newMemberList: List<Member>) {
-        val memberDiffUtil = MemberListDiffUtil(membersList ,newMemberList)
-
-        val diffResult = DiffUtil.calculateDiff(memberDiffUtil)
-
-        membersList = newMemberList
-
-        diffResult.dispatchUpdatesTo(this)
     }
 }
--- members-presentation/src/main/java/com/app/members/MembersListFragment.kt
+++ members-presentation/src/main/java/com/app/members/MembersListFragment.kt
@@ -4,23 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import com.app.members_domain.model.Member
 import com.app.teamly.members_presentation.R
 import com.app.teamly.members_presentation.databinding.FragmentMembersListBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
-import timber.log.Timber
 
 @AndroidEntryPoint
-class MembersListFragment : Fragment() {
+class MembersListFragment : Fragment() , MemberListAdapter.MemberItemListener{
 
     private val memberViewModel : MembersViewModel by viewModels()
     private lateinit var membersAdapter : MemberListAdapter
@@ -31,7 +30,7 @@ class MembersListFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = DataBindingUtil.inflate(inflater,R.layout.fragment_members_list,container,false)
         return binding.root
     }
@@ -39,23 +38,20 @@ class MembersListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        membersAdapter = MemberListAdapter()
+        membersAdapter = MemberListAdapter(this)
 
-        val recyclerView = binding.rvMember
-
-        recyclerView.apply {
-            adapter = membersAdapter
-            layoutManager = LinearLayoutManager(requireContext())
-        }
+        binding.memberListAdapter = membersAdapter
 
         viewLifecycleOwner.lifecycleScope.launch {
             repeatOnLifecycle(Lifecycle.State.STARTED){
                 memberViewModel.memberState.collectLatest { memberItem ->
-                    membersAdapter.setMembers(memberItem.members)
+                    membersAdapter.submitList(memberItem.members)
                 }
             }
         }
+    }
 
-
+    override fun onMemberItemClicked(currentItem: Member) {
+        Toast.makeText(requireContext(), "${currentItem.name} clicked" , Toast.LENGTH_SHORT).show()
     }
 }
